f10276d6ef052474edb6ed5998a8b1c697094254
ehrhardt98 <jorgeavbe@al.insper.edu.br>
2018-04-13 17:37:44 -0300

    LED_pronto

--- src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/view/GateView.java
+++ src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/view/GateView.java
@@ -1,16 +1,21 @@
 package br.pro.hashi.ensino.desagil.rafaelogic.view;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
-
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import java.util.LinkedList;
 
 import javax.swing.JCheckBox;
+import javax.swing.JColorChooser;
 
 import br.pro.hashi.ensino.desagil.rafaelogic.model.Gate;
 import br.pro.hashi.ensino.desagil.rafaelogic.model.Source;
 
-public class GateView extends SimplePanel implements ActionListener {
+public class GateView extends SimplePanel implements ActionListener, MouseListener {
 
 	private static final long serialVersionUID = 1L;
 
@@ -19,24 +24,26 @@ public class GateView extends SimplePanel implements ActionListener {
 	private JCheckBox saida;
 	private LinkedList<JCheckBox> checkBoxes;
 	private LinkedList<Integer> y;
+	private Color color;
+	private int cor;
 
 	public GateView(Gate gate) {
 		super(300, 300);
-		
+
 		this.gate = gate;
-		
+
 		sources = new LinkedList<>();
 		checkBoxes = new LinkedList<>();
 		y = new LinkedList<>();
 		
-		
+
 		for(int i = 0;gate.getSize() > i; i++) {
 			JCheckBox checkBox = new JCheckBox();
 			Source source = new Source();
 			checkBoxes.add(checkBox);
 			sources.add(source);
 		}
-		
+
 		saida = new JCheckBox();
 
 		if(checkBoxes.size() == 1) {
@@ -46,20 +53,22 @@ public class GateView extends SimplePanel implements ActionListener {
 			y.add(200);
 			y.add(100);
 		}
- 
+
 		for(int i = 0; checkBoxes.size() > i; i++) {
 			add(checkBoxes.get(i),45,y.get(i),18,18);
 		}
-		add(saida,245,142,18,18);
+		//add(saida,245,142,18,18);
 
-		
+
 		for(JCheckBox box: checkBoxes) {
 			box.addActionListener(this);
 		}
-		
+
 		saida.setEnabled(false);
 
 		update();
+		
+		addMouseListener(this);
 	}
 
 	private void update() {	
@@ -68,10 +77,64 @@ public class GateView extends SimplePanel implements ActionListener {
 			gate.connect(i,  sources.get(i));
 		}
 		saida.setSelected(gate.read());
+		
+		if(gate.read() == true) {
+			color = Color.RED;
+			cor = 1;
+			repaint();
+		}
+		else {
+			color = Color.BLACK;
+			cor = 0;
+			repaint();
+		}
 	}
+	
 
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		update();
 	}
+
+	@Override
+	public void mouseClicked(MouseEvent event) {
+		
+		int x = event.getX();
+		int y = event.getY();
+		double k;
+
+		k = Math.sqrt(Math.pow(255-x, 2)+Math.pow(152-y, 2));
+		
+		if(k <= 10 && cor == 1) {
+			color = JColorChooser.showDialog(this, null, color);
+			repaint();
+		}
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent event) {		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent event) {		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent event) {		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent event) {		
+	}
+
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(color);
+		g.fillOval(245, 142, 20, 20);
+
+		getToolkit().sync();
+
+	}
+
 }
